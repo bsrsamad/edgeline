ba7838d86ef00cf6d81bb0380b0df6dbc2be64af
bsrsamad <bsrabdulsamad@gmail.com>
2020-03-31 18:56:55 +0100

    starting point

---
+++ submit.php
@@ -0,0 +1,18 @@
+<?php
+ if(isset($_POST['submit'])){
+     $name=$_POST['name'];
+     $email=$_POST['email'];
+     $phone=$_POST['phone'];
+     $msg=$_POST['msg'];
+
+     $to='bsrnasir@yahoo.com';
+     $subject='form submission';
+     $message="name: ".$name."\n"."Phone: ".$phone."\n". "wrote the following: "."\n\n".$msg;
+     $headers="from: ".$email;
+
+     if(mail($to,$subject,$message,$headers)){
+         echo"<h1> sent successful thank you" ."  ".$name.",we will contact you shortly</h1>";
+     } else{
+         echo"something went wrong";
+     }
+ }
